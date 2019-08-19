a320c4388378d7c2da6b70d45bedc8a48133aa91
Alexandre Bourget <alex@bourget.cc>
2019-08-19 08:20:05 -0400

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build -t gcr.io/abourget-007/fde-mtl:latest .
+TAG=$(git rev-parse --short HEAD)
+
+docker build -t gcr.io/abourget-007/fde-mtl:latest -t gcr.io/abourget-007/fde-mtl:$TAG .
 docker push gcr.io/abourget-007/fde-mtl:latest
+docker push gcr.io/abourget-007/fde-mtl:$TAG
 
